b09fefa893e1c6ebc6d34260f9e1a837a7ee1d09
jigang <jiweigang2008@126.com>
2016-12-23 22:30:06 +0800

    work

--- src/com/example/remap/User.java
+++ src/com/example/remap/User.java
@@ -13,4 +13,14 @@ public class User {
 		System.out.println(name);
 		return name;
 	}
+	public static String delUser1(String name){
+		
+		try {
+			System.out.println(name);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return name;
+	}
 }
---
+++ src/com/example/trycatch/TryCatch.java
@@ -0,0 +1,5 @@
+package com.example.trycatch;
+
+public class TryCatch {
+
+}
